08d38d99fe17a4d1cc973111d6552abb0f475c44
DzmitrySiniakou <jack.pott@mail.ru>
2020-12-18 14:51:04 +0300

    fix: export types

--- src/components/App/index.ts
+++ src/components/App/index.ts
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import App from './App';
 import { IAppStore } from '../../interfaces/redux/appStore';
 import { setCasesData } from '../../redux/creators/appStore';
-import getCovidSummary from '../../redux/creators/appStoreEffects';
+import { getCovidSummary } from '../../redux/creators/appStoreEffects';
 
 const mapStateToProps = ({ appStore }: { appStore: IAppStore }) => ({
   appStore,
