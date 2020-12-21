c86048cc0a7384c01cf32d6cce8a3202221e25b8
Ekaterina Matveyeva <grinteabox@gmail.com>
2020-12-21 21:08:31 +0200

    feat/6/ add scroll

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC<Props> = ({ getCovidSummary, getCountryInfo }) => {
 
   return (
     <div className={appstyles.app}>
-      <header className={appstyles.app_header}>
+      <header className={`${appstyles.app_component_block} ${appstyles.app_header}`}>
         <HeaderBlock />
       </header>
       <div className={appstyles.app_main}>
